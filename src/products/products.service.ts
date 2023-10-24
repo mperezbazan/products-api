cb0e96c5247035ec196b09519fc75d05f56eb3cc
Martin Bazan <martin@superiordigital.design>
2023-10-24 12:37:00 -0300

    add products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+const DATA = {
+  products: [
+    {
+      sku: 10277,
+      name: 'Product test',
+      description: 'Product test description',
+      category: 'Category test',
+      brand: 'Brand test',
+      originalValue: 100,
+      currentValue: 10,
+      unitType: 'unidad',
+      unitQuantity: 10,
+      images: [
+        {
+          imageUrl:
+            'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+          order: 1,
+        },
+      ],
+      option1: 'Option 1',
+      values1: 'Value 1',
+      valueAddition1: 'Value addition 1',
+      option2: 'Option 2',
+      values2: 'Value 2',
+      valueAddition2: 'Value addition 2',
+    },
+  ],
+};
 @Injectable()
 export class ProductsService {
   create(createProductDto: CreateProductDto) {
@@ -9,7 +36,7 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return DATA;
   }
 
   findOne(id: number) {
