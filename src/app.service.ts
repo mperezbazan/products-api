4b2275e746efd43cbe7ed6ab95944e3d57b6953c
Martin Bazan <martin@superiordigital.design>
2023-10-24 11:43:36 -0300

    change to items

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
-const DATA = [
-  {
-    sku: 10277,
-    name: 'Product test',
-    description: 'Product test description',
-    category: 'Category test',
-    brand: 'Brand test',
-    originalValue: 100,
-    currentValue: 10,
-    unitType: 'unidad',
-    unitQuantity: 10,
-    images:
-      'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-    option1: 'Option 1',
-    values1: 'Value 1',
-    valueAddition1: 'Value addition 1',
-    option2: 'Option 2',
-    values2: 'Value 2',
-    valueAddition2: 'Value addition 2',
-  },
-];
+const DATA = {
+  items: [
+    {
+      sku: 10277,
+      name: 'Product test',
+      description: 'Product test description',
+      category: 'Category test',
+      brand: 'Brand test',
+      originalValue: 100,
+      currentValue: 10,
+      unitType: 'unidad',
+      unitQuantity: 10,
+      images:
+        'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+      option1: 'Option 1',
+      values1: 'Value 1',
+      valueAddition1: 'Value addition 1',
+      option2: 'Option 2',
+      values2: 'Value 2',
+      valueAddition2: 'Value addition 2',
+    },
+  ],
+};
 
 @Injectable()
 export class AppService {
